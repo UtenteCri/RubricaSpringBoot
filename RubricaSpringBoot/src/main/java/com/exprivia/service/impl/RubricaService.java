91433c43398ebcae5240e86c504dd38bc9115e3a
Marco Tondi <marco.tondi@exprivia.com>
2024-02-28 10:04:10 +0100

    add SQLite

--- RubricaSpringBoot/src/main/java/com/exprivia/service/impl/RubricaService.java
+++ RubricaSpringBoot/src/main/java/com/exprivia/service/impl/RubricaService.java
@@ -1,56 +1,45 @@
 package com.exprivia.service.impl;
 
-import org.springframework.stereotype.Service;
-
-import com.exprivia.entity.Contatto;
-import com.exprivia.service.InterfaceRubricaService;
-
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.exprivia.entity.Contatto;
+import com.exprivia.repository.ContattoRepository;
+import com.exprivia.service.InterfaceRubricaService;
 
 @Service
 public class RubricaService implements InterfaceRubricaService {
-
     private final static Logger LOGGER = Logger.getLogger(RubricaService.class.getName());
 
-    private List<Contatto> listaContatti;
-
-    public RubricaService() {
-        listaContatti = new ArrayList<>();
-        aggiungiContatto("Luca", "Gialli", "3491212121");
-        aggiungiContatto("Mario", "Rossi", "3385353535");
-        aggiungiContatto("Guido", "Verdi", "3316464646");
-        aggiungiContatto("Andrea", "Bianchi", "33385858585");
-        aggiungiContatto("Maria", "Bianchi", "334555585");
-    }
+    @Autowired
+    private ContattoRepository repository;
 
     @Override
     public Contatto aggiungiContatto(String nome, String cognome, String numero) {
         Contatto contatto = new Contatto(nome, cognome, numero);
         return aggiungiContatto(contatto);
-    }  
-     
+    }
+
     @Override
     public Contatto aggiungiContatto(Contatto contatto) {
-        listaContatti.add(contatto);
         LOGGER.info("Inserito contatto: " + contatto);
-        return contatto;
+        return repository.save(contatto);
     }
 
     @Override
     public List<Contatto> stampaContatti() {
-        return listaContatti;
+        return repository.findAll();
     }
 
     @Override
     public List<Contatto> cercaContattiPerCognome(String cognome) {
-        List<Contatto> listaBreveContatti = new ArrayList<>();
-        for (Contatto contatto : listaContatti) {
-            if (contatto.getCognome().equals(cognome)) {
-                listaBreveContatti.add(contatto);
-            }
-        }
-        return listaBreveContatti;
+        Optional<Contatto> opt = repository.findById(cognome);
+
+        return opt.isPresent() ? Arrays.asList(opt.get()) : new ArrayList<Contatto>();
     }
+
 }
